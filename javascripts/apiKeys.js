32d777b32e7bf7ac1cd6a563faee8aa04872523c
John Achor <johnachor@gmail.com>
2017-10-31 20:14:27 -0500

    navbar and data retrieval working

--- javascripts/apiKeys.js
+++ javascripts/apiKeys.js
@@ -1,2 +1,30 @@
 "use strict";
 
+const firebaseApi = require("./firebaseApi");
+
+const apiKeys = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax("../db/apiKeys.json")
+      .done((data) => {
+        resolve(data);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const retrieveKeys = () => {
+  apiKeys()
+    .then((results) => {
+      console.log(results);
+      firebaseApi.setKey(results.firebaseKeys);
+      firebase.initializeApp(results.firebaseKeys);
+      
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = { retrieveKeys };
--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -1,8 +1,42 @@
 "use strict";
 
 let firebaseKey = "";
-let userUid = "";
+
 
 const setKey = (key) => {
   firebaseKey = key;
+  console.log(firebaseKey);
 };
+
+const getParkData = (collection) => {
+  return new Promise((resolve, reject) => {
+    $.ajax(`${firebaseKey.databaseURL}/${collection}.json`)
+      .then((data) => {
+        if (data != null) {
+          resolve(data);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getAreas = () => {
+  return getParkData("areas");
+};
+
+const getAttractions = () => {
+  return getParkData("attractions");
+};
+
+const getAttractionTypes = () => {
+  return getParkData("attraction_types");
+};
+
+const getParkInfo = () => {
+  return getParkData("park-info");
+};
+
+
+module.exports = { setKey, getAreas, getAttractionTypes, getAttractions, getParkInfo };
