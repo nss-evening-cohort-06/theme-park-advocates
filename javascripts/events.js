f3b61f957f1b21aa2ce5afa3ebbeb2502f4d0804
John Achor <johnachor@gmail.com>
2017-11-02 16:15:55 -0700

    partway done with search box behavior

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,5 +1,27 @@
 "use strict";
+const firebaseApi = require("./firebaseApi");
+
+const checkAreaMatch = (attraction) => {
+  return attraction.name.includes($("#searchBox").val());
+};
+
+
+const highlightMatchingAreas = (e) => {
+  console.log(e);
+  if (e.key === "Enter") {
+    firebaseApi.getAttractions()
+      .then((attractions) => { return attractions.filter(checkAreaMatch); })
+      .then((filtered) => { console.log(filtered); });
+  }
+};
+
 
 const searchBox = () => {
-  
+  $("#searchBox").keypress(highlightMatchingAreas);
 };
+
+const initializeEvents = () => {
+  searchBox();
+};
+
+module.exports = { initializeEvents };
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const apiKeys = require("./apiKeys");
-const firebaseApi = require("./firebaseApi");
+const events = require("./events");
 
 console.log("in main.js");
 
 const main = () => {
   apiKeys.retrieveKeys();
-  
+  events.initializeEvents();
 };
 
 main();
