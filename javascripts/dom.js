b3290b3bbb8a94110f8fac3b840e1193b7143954
Jess Brawner <jessnbillbrawner@gmail.com>
2017-11-02 18:29:38 -0500

    wrote promise to retrieve attractions by area, dom function to print to list container, and event

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,19 +1,21 @@
 "use strict";
 
-let attractions = require('./attractions');
-
-
-let domString = () => {
+// Prints all attractions with an area_id matching the area_id of e.target to the list container:
+let printAttractionsArray = (attractionsWithTypes) => {
 	let domString = '';
-	for(let i = 0; )
+	for(let i = 0; i < attractionsWithTypes.length; i++) {
 	domString += `<div>
-					<a href=""><h4 class="">${attractions[i].name}</a> (${attractions[i].type_name})</h4>
+					<a href=""><h4 class="">${attractionsWithTypes[i].name}</a> (${attractionsWithTypes[i].type_name})</h4>
 					<div id="attractionDescription" class="hide">
-						<h4>${attractions[i].description}</h4>
+						<h4>${attractionsWithTypes[i].description}</h4>
 					</div>
 				  </div>`;
+	}
+	printToDom(domString);
 };
 
-let printToDom = () => {
-
+let printToDom = (strang) => {
+	$('#listContainer').html(strang);
 };
+
+module.exports = {printAttractionsArray};
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,18 +1,35 @@
 "use strict";
 
-const attractions = require('./attractions');
+const firebaseApi = require('./firebaseApi');
+const dom = require('./dom');
+console.log("dom in events", dom);
 
 const searchBox = () => {
   
 };
 
-const displayAttractionsByArea = () => {
-	let area_id;
-	
-	attractions.getAttractionsByArea(area_id).then((attractionsArray) => {
-		dom.printAttractionsArray(attractionsArray);
-				}).catch((err) => {
-				console.log(err);
+// Event to display attractions in the list container when an area is clicked in the map:	 
+$('.areas').click((e) => {
+	// Grab the area id number from the id assigned to the element in index.html:
+	let area_id = e.target.id.slice(4, 5);
+	// The following Promise.all will return an array containing 2 indexes, 0 will be an array consisting all attractions with an area_id matching the area_id of e.target (from getAttractionsByArea), 1 will be an array consisting of all types (from getAttractionsByType):
+	Promise.all([firebaseApi.getAttractionsByArea(area_id), firebaseApi.getAttractionTypes()])
+	.then((values) => {	
+		let attractionsWithTypes = [];	
+		values[0].forEach((attraction) => {
+			values[1].forEach((type) => {
+				// Conditional to add type_name key to attraction with a value equal to type.name provided that the type.id and attractions.type_id are the same:
+				if (type.id == attraction.type_id) {
+					attraction.type_name = type.name;
+					// Push the attractions that now have a key of type_name into the attractionsWithTypes array wich can then be passed into the dom function to print:
+					attractionsWithTypes.push(attraction);
+				}
 			});
+		});
+		console.log("attractionsWithTypes?", attractionsWithTypes);
+		dom.printAttractionsArray(attractionsWithTypes);
+	}).catch((err) => {
+			console.log(err);
+		});
+});
 
-};
