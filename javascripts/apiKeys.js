4d04c4e1f785ddc9ecaf73775bb3a97003f77697
John Achor <johnachor@gmail.com>
2017-11-08 21:32:06 -0600

    moved things around to get initial functions firing after firebase key has been set

--- javascripts/apiKeys.js
+++ javascripts/apiKeys.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const firebaseApi = require("./firebaseApi");
-
 const apiKeys = () => {
   return new Promise((resolve, reject) => {
     $.ajax("../db/apiKeys.json")
@@ -14,16 +12,6 @@ const apiKeys = () => {
   });
 };
 
-const retrieveKeys = () => {
-  apiKeys()
-    .then((results) => {
-      firebaseApi.setKey(results.firebaseKeys);
-      firebase.initializeApp(results.firebaseKeys);
-      // Demonstrates that each of the four possible Firebase requests are working
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
 
-module.exports = { retrieveKeys };
+
+module.exports = { apiKeys };
--- javascripts/main.js
+++ javascripts/main.js
@@ -2,10 +2,22 @@
 
 const apiKeys = require("./apiKeys");
 const events = require("./events");
+const firebaseApi = require("./firebaseApi");
 
 const main = () => {
-  apiKeys.retrieveKeys();
-  events.initializeEvents();
+  console.log(firebaseApi);
+  apiKeys.apiKeys()
+    .then((results) => {
+      console.log(results);
+      console.log(firebaseApi.setKey);
+      firebaseApi.setKey(results.firebaseKeys);
+      firebase.initializeApp(results.firebaseKeys);
+      firebaseApi.getAreas();
+      firebaseApi.getHoursOfOperation();
+      firebaseApi.attractionsWithAreaName();
+      events.initializeEvents();
+    });
+
 };
 
 main();
